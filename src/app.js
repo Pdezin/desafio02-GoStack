0d12e0da6a359987e5eed1f54b56a8dffb722ecf
Pedro Jardim <pedro200219@outlook.com>
2021-09-26 17:37:25 -0300

    finalizado

--- src/app.js
+++ src/app.js
@@ -65,19 +65,22 @@ app.delete("/repositories/:id", (request, response) => {
   const indexRepository = repositories.findIndex(
     (repository) => repository.id === id
   );
-  if (indexRepository < 0) return response.status(404).json({ error: "Not found" });
+  if (indexRepository < 0)
+    return response.status(404).json({ error: "Not found" });
   repositories.splice(indexRepository, 1);
   return response.status(204).send();
 });
 
 //POST LIKE
 app.post("/repositories/:id/like", (request, response) => {
-  const {id} = request.params;
+  const { id } = request.params;
   const indexRepository = repositories.findIndex(
     (repository) => repository.id === id
   );
-  if (indexRepository < 0) return response.status(404).json({ error: "Not found" });
-  repositories[indexRepository].likes = Number(repositories[indexRepository].likes) + 1;
+  if (indexRepository < 0)
+    return response.status(404).json({ error: "Not found" });
+  repositories[indexRepository].likes =
+    Number(repositories[indexRepository].likes) + 1;
   return response.json(repositories[indexRepository]);
 });
 
